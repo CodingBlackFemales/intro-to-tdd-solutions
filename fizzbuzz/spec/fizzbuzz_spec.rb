75f996999457631e420a715675d2d7dc3c540fee
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-22 10:18:20 +0100

    Add test for: returns `Buzz` for multiples of 5

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'fizzbuzz' do
   it 'returns `Fizz` for multiples of 3' do
     expect(fizzbuzz(6)).to eq('Fizz')
   end
+
+  it 'returns `Buzz for multiples of 5' do
+    expect(fizzbuzz(10)).to eq('Buzz')
+  end
 end
