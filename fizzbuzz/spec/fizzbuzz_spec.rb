76bba38223768eaf78c5438cf55c035267f2de42
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-22 10:14:05 +0100

    Add test for: returns `Fizz` for multiples of 3

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 RSpec.describe 'fizzbuzz' do
-  it 'Returns nil' do
-    expect(fizzbuzz(3)).to eq(nil)
+  it 'returns `Fizz` for multiples of 3' do
+    expect(fizzbuzz(6)).to eq('Fizz')
   end
 end
