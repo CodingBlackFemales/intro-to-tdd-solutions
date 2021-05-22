7ce83c15f7cd957cc95fd842d16cd3a10f3c490b
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-22 18:55:57 +0100

    Add test: returns false when the year is divisible by 100, but not 400

--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'leap_year' do
   it "returns true when the year is divisible by 400" do
     expect(leap_year(2000)).to eq(true)
   end
+
+  it 'returns false when the year is divisible by 100, but not 400' do
+    expect(leap_year(1700)).to eq(false)
+  end
 end
