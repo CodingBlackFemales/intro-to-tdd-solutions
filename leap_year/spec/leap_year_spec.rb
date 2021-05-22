0f070641f3d45cf24103c9795ebbfa677035fa53
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-22 19:32:52 +0100

    Additional testing: add examples of testing numerous years using loops

--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -16,4 +16,43 @@ RSpec.describe 'leap_year' do
   it 'returns false when the year is not divisible by 4' do
     expect(leap_year(2017)).to eq(false)
   end
+
+  # Examples of how to test numerous years using loops
+
+  context 'divisible by 400' do
+    divisible_by_400 = [1600, 2000]
+
+    divisible_by_400.each do |year|
+      it "returns true for #{year}" do
+        expect(leap_year(year)).to eq(true)
+      end
+    end
+  end
+
+  context 'divisible by 100, but not 400' do
+    divisible_by_100 = [1700, 1800, 1900]
+
+    divisible_by_100.each do |year|
+      it "returns false for #{year}" do
+        expect(leap_year(year)).to eq(false)
+      end
+    end
+  end
+
+  context 'divisible by 4, but not 100' do
+    divisible_by_4 = [2008, 2012, 2016]
+    not_divisible_by_4 = [2017, 2018, 2019]
+
+    divisible_by_4.each do |year|
+      it "returns true for #{year}" do
+        expect(leap_year(year)).to eq(true)
+      end
+    end
+
+    not_divisible_by_4.each do |year|
+      it "returns false for #{year}" do
+        expect(leap_year(year)).to eq(false)
+      end
+    end
+  end
 end
