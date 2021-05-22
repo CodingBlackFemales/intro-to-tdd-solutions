c48feaf8bd22b76aa963a48c5766fb6c9aae873e
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-22 19:18:56 +0100

    Refactor: extract not divisible by 100 conditional to is_not_divisible_by_100? method

--- leap_year/lib/leap_year.rb
+++ leap_year/lib/leap_year.rb
@@ -1,7 +1,11 @@
 def leap_year(year)
-  is_divisible_by_400?(year) || year % 100 != 0 && year % 4 == 0
+  is_divisible_by_400?(year) || is_not_divisible_by_100?(year) && year % 4 == 0
 end
 
 def is_divisible_by_400?(year)
   year % 400 == 0
+end
+
+def is_not_divisible_by_100?(year)
+  year % 100 != 0
 end
