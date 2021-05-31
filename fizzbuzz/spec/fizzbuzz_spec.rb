f808dc2ab318bb498251e875a1701a4ffd35fb99
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-31 16:04:27 +0100

    Add test: returns 2 when the input is 2

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'fizzbuzz' do
   it 'returns 1 when the input is 1' do
     expect(fizzbuzz(1)).to eq(1)
   end
+
+  it 'returns 2 when the input is 2' do
+    expect(fizzbuzz(2)).to eq(2)
+  end
 end
