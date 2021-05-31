e3d7eafced1db62578230606856bf0ec577a9ff1
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-31 17:06:08 +0100

    Add test: returns true when the year is 2000

--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -1,7 +1,9 @@
 require 'leap_year'
 
 RSpec.describe 'leap_year' do
-  it "returns nil" do
-    expect(leap_year(2012)).to eq(nil)
+  context 'divisible by 400' do
+    it "returns true when the year is 2000" do
+      expect(leap_year(2000)).to eq(true)
+    end
   end
 end
