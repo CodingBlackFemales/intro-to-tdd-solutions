8a057ea8d44863e48ff35598a99f3055e4bbbde2
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-31 16:14:31 +0100

    Add test: returns Fizz when the input is 3

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -1,13 +1,19 @@
 require 'fizzbuzz'
 
 RSpec.describe 'fizzbuzz' do
-  context 'returns the number' do
+  context 'returns the number:' do
     numbers = [1, 2, 4, 7, 8, 11, 13, 14]
 
     numbers.each do |number|
       it "returns #{number} when the input is #{number}" do
         expect(fizzbuzz(number)).to eq(number)
       end
+    end
+  end
+
+  context 'multiples of 3:' do
+    it "returns `Fizz` when the input is 3" do
+      expect(fizzbuzz(3)).to eq("Fizz")
     end
   end
 end
