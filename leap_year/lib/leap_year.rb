d0f1b07fccb2cd0648c9057188bdd75558aad9a6
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-05-31 17:36:58 +0100

    Refactor: extract divisible by 4 check to is_divisible_by_4?

--- leap_year/lib/leap_year.rb
+++ leap_year/lib/leap_year.rb
@@ -1,7 +1,13 @@
 def leap_year(year)
-  is_divisible_by_400?(year) || year % 100 != 0 && year % 4 == 0
+  is_divisible_by_400?(year) ||
+  year % 100 != 0 &&
+  is_divisible_by_4?(year)
 end
 
 def is_divisible_by_400?(year)
   year % 400 == 0
+end
+
+def is_divisible_by_4?(year)
+  year % 4 == 0
 end
