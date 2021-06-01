64c2afc9c9f039ee292817aa084ccbd8723e8d4e
Elle <33027271+ellehallal@users.noreply.github.com>
2021-06-01 16:51:00 +0100

    Add solution for Leap Year exercise (#6)
    * Add test: returns true when the year is 2000
    
    * Add implementation: returns true when the year is 2000
    
    * Add test: returns false when the year is 1700
    
    * Add implementation: returns false when the year is 1700
    
    * Add test: returns false when the year is 1800
    
    * Add implementation: returns false when the year is 1800
    
    * Refactor tests: add loop to test years divisible by 100
    
    * Add test: returns true when the year is 2008
    
    * Add implementation: returns true when the year is 2008
    
    * Add test: returns true when the year is 2012
    
    * Add implementation: returns true when the year is 2012
    
    * Refactor tests: add loop to test years divisible by 4
    
    * Add test: returns false when the year is 2017
    
    * Refactor tests: add loop to test years not divisible by 400, 100, or 4
    
    * Refactor: extract divisible by 400 check to is_divisible_by_400?
    
    * Refactor: extract divisible by 4 check to is_divisible_by_4?
    
    * Refactor: extract divisible by 100 check to is_divisible_by_100?
    
    * Refactor: amend assertions to use be true and be false matchers

--- leap_year/lib/leap_year.rb
+++ leap_year/lib/leap_year.rb
@@ -1,3 +1,17 @@
 def leap_year(year)
-  nil
+  is_divisible_by_400?(year) ||
+  is_divisible_by_4?(year) &&
+  !is_divisible_by_100?(year)
+end
+
+def is_divisible_by_400?(year)
+  year % 400 == 0
+end
+
+def is_divisible_by_4?(year)
+  year % 4 == 0
+end
+
+def is_divisible_by_100?(year)
+  year % 100 == 0
 end
--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -1,7 +1,55 @@
 require 'leap_year'
 
 RSpec.describe 'leap_year' do
-  it "returns nil" do
-    expect(leap_year(2012)).to eq(nil)
+  context 'divisible by 400:' do
+    it "returns true when the year is 2000" do
+      expect(leap_year(2000)).to be true
+    end
+  end
+
+  context 'divisible by 100, but not 400:' do
+    divisible_by_100 = [1700, 1800, 1900]
+
+    divisible_by_100.each do |number|
+      it "returns false when the year is #{number}" do
+        expect(leap_year(number)).to be false
+      end
+    end
+  end
+
+  context 'divisible by 4, but not 100:' do
+    divisible_by_4 = [2004, 2008, 2012, 2016, 2020]
+
+    divisible_by_4.each do |number|
+      it "returns true when the year is #{number}" do
+        expect(leap_year(number)).to be true
+      end
+    end
+  end
+
+  context 'not divisible by 400, 100 or 4:' do
+    not_divisible = [
+      2001,
+      2002,
+      2003,
+      2005,
+      2006,
+      2007,
+      2009,
+      2010,
+      2011,
+      2013,
+      2014,
+      2015,
+      2017,
+      2018,
+      2019
+    ]
+
+    not_divisible.each do |number|
+      it "returns false when the year is #{number}" do
+        expect(leap_year(number)).to be false
+      end
+    end
   end
 end
